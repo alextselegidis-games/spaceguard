780de86ab59bb8c34f02eacd1b418f344584fcde
alext <alextselegidis@gmail.com>
2017-08-06 12:50:52 +0200

    Added file banner comment blocks.

--- scripts/Bomb.js
+++ scripts/Bomb.js
@@ -1,3 +1,13 @@
+/* ----------------------------------------------------------------------------
+ * Spaceguard - Arcade Space Game written in JavaScript
+ *
+ * @package     Spaceguard
+ * @author      A.Tselegidis <alextselegidis@gmail.com>
+ * @copyright   Copyright (c) 2013 - 2017, Alex Tselegidis
+ * @license     http://opensource.org/licenses/GPL-3.0 - GPLv3
+ * @link        http://alextselegidis.com/spaceguard
+ * ---------------------------------------------------------------------------- */
+
 import {SCALE, OBJ_TYPE_BOMB} from './Constants';
 
 /**
